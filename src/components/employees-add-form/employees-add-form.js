83f0628998d432536035a894464d3c493226851e
Viktor590 <resident1081994@gmail.com>
2021-10-30 12:44:21 +0300

    added functional delete and added new item

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -18,10 +18,12 @@ class EmployeesAddForm extends Component {
     })
   }
 
-
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.state.name, this.state.salary)
+  }
 
   render() {
-
 
     const { name, salary } = this.state;
 
@@ -29,7 +31,8 @@ class EmployeesAddForm extends Component {
       <div className="app-add-form">
         <h3>Добавьте нового сотрудника</h3>
         <form
-          className="add-form d-flex">
+          className="add-form d-flex"
+          onSubmit={this.onSubmit}>
           <input type="text"
             className="form-control new-post-label"
             placeholder="Как его зовут?"
